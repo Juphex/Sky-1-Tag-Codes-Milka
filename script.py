8de1958bfe8733c0be395a28a75d6f322934e460
Juphex <20795522+Juphex@users.noreply.github.com>
2019-02-17 11:03:37 +0100

    improvement

--- script.py
+++ script.py
@@ -4,20 +4,25 @@ import random as rand
 
 zaehl = 1000
 for i in range (0, 10000):
-    f = open("milkacodes.txt", "a")
+    f = open("skytickets.txt", "a")
     r = rq.post("https://sky.milka.de/Service/Participate", data = {
-        "Code1": "OOV028" + str(zaehl) + "",
-    "Code2": "OOV028" + str(zaehl + 1) + "",
+        #"Code1": "OOV028" + str(zaehl) + "",
+    #"Code2": "OOV028" + str(zaehl + 1) + "",
+    #makes use of milka not blocking codes
+    "Code1": "OOV0281034",
+    "Code2": "OOV0281033",
     "FirstName": "asdas",
     "LastName": "asdasd",
     "Birthday": "04.04.1957",
     "Country":"" ,
+    #random email
     "Email": str(zaehl) + "@" + str(zaehl * rand.random()) + ".de",
     "TermsOfUse": "true",
     })
-    zaehl += 2
+    #zaehl += 2
     data = js.loads(r.text)
     d = data["metadata"]
+    #write ticket to txt
     try:
         print (data)
         f.write(d["ticket"] + "\n")
